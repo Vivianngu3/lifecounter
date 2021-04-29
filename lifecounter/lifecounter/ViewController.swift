5a074100ad11bd85dc36e0362a2cad3b3cfca679
“vivianngu3” <vn23@uw.edu>
2021-04-28 21:17:17 -0700

    life counter 2, partially done

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -7,120 +7,131 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+ 
+var history:[String] = []
 
-    @IBOutlet weak var p1Life: UILabel!
-    @IBOutlet weak var p2Life: UILabel!
-    @IBOutlet weak var p3Life: UILabel!
-    @IBOutlet weak var p4Life: UILabel!
-    @IBOutlet weak var playerLose: UILabel!
-    
-    
-    func lifeCount (value: Int, player: Int) {
-            switch player {
-                case 1:
-                    let totalLife = Int(p1Life.text!)!
-                    let newLife = totalLife + value
-                    p1Life.text = String(newLife)
-                    if (newLife <= 0) {
-                        playerLose.text = "Player 1 Loses!"
-                    }
-                case 2:
-                    let totalLife = Int(p2Life.text!)!
-                    let newLife = totalLife + value
-                    p2Life.text = String(newLife)
-                    if (newLife <= 0) {
-                        playerLose.text = "Player 2 Loses!"
-                    }
-                case 3:
-                    let totalLife = Int(p3Life.text!)!
-                    let newLife = totalLife + value
-                    p3Life.text = String(newLife)
-                    if (newLife <= 0) {
-                        playerLose.text = "Player 3 Loses!"
-                    }
-                case 4:
-                    let totalLife = Int(p4Life.text!)!
-                    let newLife = totalLife + value
-                    p4Life.text = String(newLife)
-                    if (newLife <= 0) {
-                        playerLose.text = "Player 4 Loses!"
-                    }
-            default:
-                print("non-existant")
-                
-            }
+protocol gameSettings {
+    func startingGame()
+}
+
+class ViewController: UIViewController, gameSettings {
+
+    @IBOutlet weak var players: UIStackView!
+    @IBOutlet weak var addPlayerButton: UIButton!
+    var count = 1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
     
+    @IBAction func addPlayers(_ sender: Any) {
+        let name = "Player \(count)"
+        let life = 20
+        let playerView = PlayerView()
+        playerView.data = (name, life)
+        players.addArrangedSubview(playerView)
+        count += 1
+        if count == 9 {
+            addPlayerButton.isEnabled = false
+        }
+    }
+    
+    func startingGame() {
+        addPlayerButton.isEnabled = false
+    }
+}
+
+class PlayerView: UIView {
+    
+    var data : (String, Int) = ("", -1) {
+        didSet {
+            // Update the label with modified data
+            label.text = "\(data.0) : \(data.1)"
+        }
+    }
+
+    weak var label : UILabel!
+    weak var minus5button : UIButton!
+    weak var minus1button : UIButton!
+    weak var plus1button : UIButton!
+    weak var plus5button : UIButton!
+    var delegate: gameSettings?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.initSubViews()
+    }
+    
+    private func initSubViews() {
+        let nib = UINib(nibName: "PlayerPanel", bundle: nil)
+        let nibInstance = nib.instantiate(withOwner: self, options: nil)
+        
+        let nibView = (nibInstance.first) as! UIView
+        addSubview(nibView)
+        
+        label = (nibView.subviews[0].subviews[0] as! UILabel)
+        label.text = "\(data.0) : \(data.1)"
+        
+        minus5button = (nibView.subviews[0].subviews[1] as! UIButton)
+        minus5button.addTarget(self, action: #selector(minus5(_:)), for: .touchUpInside)
+        
+        minus1button = (nibView.subviews[0].subviews[2] as! UIButton)
+        minus1button.addTarget(self, action: #selector(minus1(_:)), for: .touchUpInside)
+        
+        plus1button = (nibView.subviews[0].subviews[3] as! UIButton)
+        plus1button.addTarget(self, action: #selector(plus1(_:)), for: .touchUpInside)
+        
+        plus5button = (nibView.subviews[0].subviews[4] as! UIButton)
+        plus5button.addTarget(self, action: #selector(plus5(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func minus5(_ sender : UIButton) {
+        data = (data.0, data.1 - 5 )
+        label.text = "\(data.0) : \(data.1)"
+        history.append("5 subtracted from \(data.0)")
+        delegate!.startingGame()
+    }
+    
+    @objc private func minus1(_ sender : UIButton) {
+        data = (data.0, data.1 - 1)
+        label.text = "\(data.0) : \(data.1)"
+        history.append("1 subtracted from \(data.0)")
+        delegate!.startingGame()
+    }
+    
+    @objc private func plus1(_ sender : UIButton) {
+        data = (data.0, data.1 + 1 )
+        label.text = "\(data.0) : \(data.1)"
+        history.append("1 added to \(data.0)")
+        delegate!.startingGame()
+    }
+    
+    @objc private func plus5(_ sender : UIButton) {
+        data = (data.0, data.1 + 5 )
+        label.text = "\(data.0) : \(data.1)"
+        history.append("5 added to \(data.0)")
+        delegate!.startingGame()
+    }
+}
+
+class HistoryView: UIViewController {
+
+    @IBOutlet weak var historyLogs: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-    @IBAction func minusFiveP1(_ sender: Any) {
-        lifeCount(value: -5, player: 1)
-    }
-    
-    @IBAction func minusOneP1(_ sender: Any) {
-        lifeCount(value: -1, player: 1)
-    }
-   
-    @IBAction func plusOneP1(_ sender: Any) {
-        lifeCount(value: +1, player: 1)
-    }
-    
-    @IBAction func plusFiveP1(_ sender: Any) {
-        lifeCount(value: +5, player: 1)
-    }
-    
-    @IBAction func minusFiveP2(_ sender: Any) {
-        lifeCount(value: -5, player: 2)
-    }
-    
-    @IBAction func minusOneP2(_ sender: Any) {
-        lifeCount(value: -1, player: 2)
-    }
-    
-    @IBAction func plusOneP2(_ sender: Any) {
-        lifeCount(value: +1, player: 2)
-    }
-    
-    @IBAction func plusFiveP2(_ sender: Any) {
-        lifeCount(value: +5, player: 2)
-    }
-    
-    @IBAction func minusFiveP3(_ sender: Any) {
-        lifeCount(value: -5, player: 3)
-    }
-    
-    @IBAction func minusOneP3(_ sender: Any) {
-        lifeCount(value: -1, player: 3)
-    }
-    
-    @IBAction func plusOneP3(_ sender: Any) {
-        lifeCount(value: +1, player: 3)
-    }
-    
-    @IBAction func plusFiveP3(_ sender: Any) {
-        lifeCount(value: +5, player: 3)
-    }
-    
-    @IBAction func minusFiveP4(_ sender: Any) {
-        lifeCount(value: -5, player: 4)
-    }
-    
-    @IBAction func minusOneP4(_ sender: Any) {
-        lifeCount(value: -1, player: 4)
-    }
-    
-    @IBAction func plusOneP4(_ sender: Any) {
-        lifeCount(value: +1, player: 4)
-    }
-    
-    @IBAction func plusFiveP4(_ sender: Any) {
-        lifeCount(value: +5, player: 4)
-    }
-    
 }
+
 
