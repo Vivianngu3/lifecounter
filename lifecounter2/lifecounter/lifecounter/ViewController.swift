2a7a54097ee7c3324074a4c8a9e22b8404737246
“vivianngu3” <vn23@uw.edu>
2021-04-29 20:43:04 -0700

    mostly done

--- lifecounter2/lifecounter/lifecounter/ViewController.swift
+++ lifecounter2/lifecounter/lifecounter/ViewController.swift
@@ -14,35 +14,59 @@ protocol gameSettings {
 }
 
 class ViewController: UIViewController, gameSettings {
-
+    
+   
     @IBOutlet weak var players: UIStackView!
     @IBOutlet weak var addPlayerButton: UIButton!
-    var count = 1
+    @IBOutlet weak var minusPlayerButton: UIButton!
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        while count < 4 {
+            count += 1
+            let player = PlayerView()
+            player.data = ("Player \(count)", 20)
+            player.tag = count
+            players.addArrangedSubview(player)
+        }
     }
     
     @IBAction func addPlayers(_ sender: Any) {
-        let name = "Player \(count)"
-        let life = 20
-        let playerView = PlayerView()
-        playerView.data = (name, life)
-        players.addArrangedSubview(playerView)
         count += 1
-        if count == 9 {
+        let player = PlayerView()
+        player.data = ("Player \(count)", 20)
+        players.addArrangedSubview(player)
+        
+        if count == 8 {
             addPlayerButton.isEnabled = false
+        }
+        if count != 2 {
+            minusPlayerButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func minusPlayer(_ sender: Any) {
+        players.arrangedSubviews[count - 1].removeFromSuperview()
+        count -= 1
+        if count != 8 {
+            addPlayerButton.isEnabled = true
+        }
+        if count == 2 {
+            minusPlayerButton.isEnabled = false
         }
     }
     
     func startingGame() {
         addPlayerButton.isEnabled = false
+        minusPlayerButton.isEnabled = false
         
     }
 }
 
 class PlayerView: UIView {
+    var delegate: gameSettings?
     
     var data : (String, Int) = ("", -1) {
         didSet {
@@ -56,7 +80,7 @@ class PlayerView: UIView {
     weak var minus1button : UIButton!
     weak var plus1button : UIButton!
     weak var plus5button : UIButton!
-    var setting: gameSettings?
+    
     
     
     override init(frame: CGRect) {
@@ -68,7 +92,6 @@ class PlayerView: UIView {
         super.init(coder: coder)
         self.initSubViews()
     }
-    
   
     private func initSubViews() {
         let nib = UINib(nibName: "PlayerPanel", bundle: nil)
@@ -91,42 +114,40 @@ class PlayerView: UIView {
         
         plus5button = (nibView.subviews[0].subviews[4] as! UIButton)
         plus5button.addTarget(self, action: #selector(plus5(_:)), for: .touchUpInside)
-        
        
     }
     
     @objc private func minus5(_ sender : UIButton) {
+        delegate?.startingGame()
         data = (data.0, data.1 - 5 )
         label.text = "\(data.0) : \(data.1)"
-        history.append("5 subtracted from \(data.0)")
-        if data.1 > 20 || data.1 < 20 {
-           
-        }
+        history.append("Player \(data.0) lost 5 lives")
+        
     }
     
     
     @objc private func minus1(_ sender : UIButton) {
+        delegate?.startingGame()
         data = (data.0, data.1 - 1)
         label.text = "\(data.0) : \(data.1)"
-        history.append("1 subtracted from \(data.0)")
+        history.append("Player \(data.0) lost 1 life")
         
     }
     
     @objc private func plus1(_ sender : UIButton) {
+        delegate?.startingGame()
         data = (data.0, data.1 + 1 )
         label.text = "\(data.0) : \(data.1)"
-        history.append("1 added to \(data.0)")
+        history.append("Player \(data.0) gained 1 life")
         
     }
     
     @objc private func plus5(_ sender : UIButton) {
+        delegate?.startingGame()
         data = (data.0, data.1 + 5 )
         label.text = "\(data.0) : \(data.1)"
-        history.append("5 added to \(data.0)")
-      
+        history.append("Player \(data.0) gained 5 lives")
     }
-    
-   
 }
 
 class HistoryView: UIViewController, UITableViewDataSource {
